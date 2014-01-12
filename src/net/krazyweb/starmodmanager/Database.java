709a149915a4d010ef306d059dfc8414fc6f6ff7
KrazyTheFox <traveler@krazyweb.net>
2014-01-12 13:27:07 -0500

    Temp commit to change branches again.

--- src/net/krazyweb/starmodmanager/Database.java
+++ src/net/krazyweb/starmodmanager/Database.java
@@ -1,10 +1,33 @@
 package net.krazyweb.starmodmanager;
 
+import java.io.File;
 import java.util.HashSet;
+
+import org.tmatesoft.sqljet.core.SqlJetTransactionMode;
+import org.tmatesoft.sqljet.core.table.SqlJetDb;
 
 public class Database {
 	
+	private static final String DATABASE_NAME = "database";
+	
+	private static SqlJetDb database;
+	
 	public static void initialize() {
+		
+		File dbFile = new File(DATABASE_NAME);
+		
+		database = SqlJetDb.open(dbFile, true);
+		
+		if (!database.getOptions().isAutovacuum()) {
+			database.getOptions().setAutovacuum(true);
+		}
+		
+		if (!tableExists(TABLE_NAME)) {
+			createTables();
+		}
+
+		database.beginTransaction(SqlJetTransactionMode.WRITE);
+		database.commit();
 		
 	}
 	
